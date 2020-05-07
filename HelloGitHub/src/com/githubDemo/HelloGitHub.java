5e5874a497e506b5546850a7bcc9045a28499e46
sangu <bsangamesh94@gmail.com>
2020-05-08 00:58:26 +0530

    Check out test

--- HelloGitHub/src/com/githubDemo/HelloGitHub.java
+++ HelloGitHub/src/com/githubDemo/HelloGitHub.java
@@ -4,8 +4,6 @@ public class HelloGitHub {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Github");
-		System.out.println("Hello Github");
-		System.out.println("Hello Github");
 	}
 
 }
